dce39d2a46c7066f533aa0d7d24465d23abaa33a
sanjit <sanjit.est14@gmail.com>
2026-06-13 22:37:54 +0600

    Creating Matches Table.

--- QUERY.sql
+++ QUERY.sql
@@ -25,16 +25,20 @@ CREATE TABLE Users (
 -- =========================================================================
 -- 2. CREATE MATCHES TABLE
 -- =========================================================================
+
 CREATE TABLE Matches (
-    match_id TYPE,
-    fixture TYPE,
-    tournament_category TYPE,
-    base_ticket_price TYPE,
-    match_status TYPE,
-    
-    -- Write your constraint to make 'match_id' the Primary Key
-    -- Write your check constraint to prevent negative ticket prices
-    -- Write your check constraint to restrict 'match_status' values
+  match_id SERIAL PRIMARY KEY,
+  fixture varchar(150) NOT NULL,
+  tournament_category varchar(100) NOT NULL,
+  base_ticket_price decimal(10, 2) NOT NULL CHECK (base_ticket_price >= 0),
+  match_status varchar(20) NOT NULL CHECK (
+    match_status IN (
+      'Available',
+      'Selling Fast',
+      'Sold Out',
+      'Postponed'
+    )
+  )
 );
 
 -- =========================================================================
